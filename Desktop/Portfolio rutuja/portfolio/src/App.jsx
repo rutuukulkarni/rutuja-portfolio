cd4537b39d4e49b278eb642265135aeaa455184c
rutuukulkarni <rutuja.kulkarni@infoprosys.co.in>
2025-11-08 15:03:43 +0530

    exp path

--- Desktop/Portfolio rutuja/portfolio/src/App.jsx
+++ Desktop/Portfolio rutuja/portfolio/src/App.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import Contact from './pages/Contact'
-import Experience from './pages/experience'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Experience from "./pages/Experience";
 
 export default function App() {
-return (
-<div className="min-h-screen bg-gradient-to-b from-white via-slate-50 to-slate-100 text-slate-900">
-<Navbar />
-<main className="max-w-6xl mx-auto px-4 md:px-6 py-8">
-<Routes>
-<Route path="/" element={<Home />} />
-<Route path="/projects" element={<Projects />} />
-<Route path="/experience" element={<Experience />} />
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-white via-slate-50 to-slate-100 text-slate-900">
+      <Navbar />
+      <main className="max-w-6xl mx-auto px-4 md:px-6 py-8">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience/>} />
 
-<Route path="/contact" element={<Contact />} />
-</Routes>
-</main>
-</div>
-)
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
